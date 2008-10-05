a550e6f76f0776988365d52633183d70db5135fe
Sven Fuchs <svenfuchs@artweb-design.de>
2008-10-05 14:28:13 +0200

    stop sprint_select_data_json from breaking when no project is given

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def sprint_select_data_json
-    Hash[*@project.sprints.all(:order => :name).map{|s| [s.id, s.name]}.flatten].to_json
+    Hash[*@project.sprints.all(:order => :name).map{|s| [s.id, s.name]}.flatten].to_json if @project
   end
 end
