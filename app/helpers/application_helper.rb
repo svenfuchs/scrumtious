c392d90494100b274f5e0233a0a2ac357ada7b9f
Sven Fuchs <svenfuchs@artweb-design.de>
2008-10-05 20:35:01 +0200

    inline selects for releases and assignees

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def release_select_data_json
+    Hash[*@project.releases.all(:order => :name).map{|s| [s.id, s.name]}.flatten].to_json if @project
+  end
+
   def sprint_select_data_json
     Hash[*@project.sprints.all(:order => :name).map{|s| [s.id, s.name]}.flatten].to_json if @project
+  end
+  
+  def user_select_data_json
+    Hash[*User.all(:order => :name).map{|s| [s.id, s.first_name]}.flatten].to_json
   end
 end
