8728af439256874e516d79ee6542065b8cd141cc
Sven Fuchs <svenfuchs@artweb-design.de>
2008-10-07 09:15:36 +0200

    do not sync local tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,6 +6,9 @@ class TicketsController < ApplicationController
   
   def show
   end
+
+  def new
+  end
   
   def create
     if @ticket.save
--- app/models/ticket_pusher.rb
+++ app/models/ticket_pusher.rb
@@ -2,7 +2,7 @@ class TicketPusher < ActionController::Caching::Sweeper
   observe Ticket
   
   def after_save(ticket)
-    Synchronizer.new(ticket.project_id).push_ticket(ticket)
+    Synchronizer.new(ticket.project_id).push_ticket(ticket) unless ticket.local?
   end
 end
 
